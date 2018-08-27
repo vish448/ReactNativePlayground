cce25915a7a231466ea783fd41b3ae918c9ce7ec
vishang soni <vishangsoni@gmail.com>
2018-08-26 22:27:36 -0400

    Test with image and flexbox to align item for the initial screen

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,Image, Dimensions} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,15 +16,47 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+var {width,height} = Dimensions.get('window');
+console.log(width);
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+       <View style={{flex: 1,
+                     justifyContent: 'flex-end',
+                     alignItems: 'center',
+                     width: width,
+                     backgroundColor: '#F8FBFC',
+                     borderBottomLeftRadius: 40,
+                     borderBottomRightRadius: 40,
+                     paddingBottom: 30,}}>
+         <Text style={styles.welcome}>Hi There! Welcome to</Text>
+         <Image style={{resizeMode: 'center'}} source={require('./img/logo.png')} />
+       </View>
+       <View style={{flex: 1,ustifyContent: 'center',alignItems: 'center', width:width}}>
+         <Greetings name='Vishang Soni' styles={styles.instructions}/>
+         <Text style={styles.instructions}>
+           Very simple Things To-Do List.  Helps you
+         </Text>
+         <Text style={styles.instructions}>
+           to manage yourdaily life, without any
+         </Text>
+         <Text style={styles.instructions}>
+           hassle!
+         </Text>
+       </View>
+    </View>
+
+    );
+  }
+}
+
+class Greetings extends Component {
+  render(){
+    return(
+      <Text styles={styles.instructions}>Hello,{this.props.name}</Text>
     );
   }
 }
@@ -34,7 +66,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFF',
+    alignSelf: 'stretch'
   },
   welcome: {
     fontSize: 20,
@@ -42,7 +75,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
+    //textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
